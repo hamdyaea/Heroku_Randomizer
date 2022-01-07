cec3ea06bb74b6cafa424a0047155388acbc2dad
hamdyaea <hamdy.aea@protonmail.com>
2022-01-07 19:15:08 +0100

    add execution counter

--- manage.py
+++ manage.py
@@ -12,5 +12,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # if it's the first time. visitors.txt must contain at least 0.
+    countfile = "visitors.txt"
+
+    f = open(countfile, "r")
+    fileContent = f.read()
+    count = int(fileContent) + 1
+    f.close()
+
+    f = open(countfile, "w")
+    f.write(str(count))
+    f.close()
+
+    f = open(countfile,"r")
+    counter = f.read()
+    f.close()
+
     random_number = random.randint(1, 1000)
-    return render_template('index.html', random_number=random_number)
+    return render_template('index.html', random_number=random_number, counter=counter)
