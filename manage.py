6542f4852b4d168d1935c072fb3f9bc50d818317
hamdyaea <hamdy.aea@protonmail.com>
2022-01-08 14:04:34 +0100

    visitors saved in dropbox

--- manage.py
+++ manage.py
@@ -6,6 +6,50 @@
 
 from flask import Flask, render_template, request
 import random 
+import os
+import dropbox
+
+drkey = os.environ["dropkey"]
+
+dbx = dropbox.Dropbox(drkey)
+
+filename = 'visitors.csv'
+
+
+def up():
+    with open(filename, "rb") as f:
+        dbx.files_upload(f.read(),'/'+filename)
+
+def down():
+    dbx.files_download_to_file(filename,"/"+filename)
+
+def deleter():
+    dbx.files_delete("/"+filename)
+
+def main():
+    try:
+        down()
+    except:
+        pass
+
+    f = open(filename, "r")
+    value = f.read()
+
+    counter = int(value)  + 1
+
+    try:
+        deleter()
+    except:
+        pass
+
+    f = open(filename, "w")
+    f.write(str(counter))
+    f.close()
+
+    up()
+
+    
+
 
 app = Flask(__name__)
 
@@ -15,6 +59,8 @@ def index():
     #yourip = request.remote_addr
     yourip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     
+    main()
+    """
     # if it's the first time. visitors.txt must contain at least 0.
     countfile = "visitors.txt"
 
@@ -30,6 +76,6 @@ def index():
     f = open(countfile,"r")
     counter = f.read()
     f.close()
-
+    """
     random_number = random.randint(1, 1000)
     return render_template('index.html', random_number=random_number, counter=counter, yourip=yourip)
