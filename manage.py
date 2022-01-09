9e77bf1720e562718ee9271b056c012469332a98
hamdyaea <hamdy.aea@protonmail.com>
2022-01-09 13:03:57 +0100

    add country list

--- manage.py
+++ manage.py
@@ -13,56 +13,71 @@ import requests
 
 drkey = os.environ["dropkey"]
 
+# for test only
+#drkey = "XXX"
+
+
 dbx = dropbox.Dropbox(drkey)
 
 filename = "visitors.csv"
-
+countryfile = "countrylist.txt"
 
 def geo():
     global ipcountry
-    ip = "83.222.156.104"
     backadrr = "https://www.ipinfo.io/"
-    fulladd = backadrr + str(ip)
+    fulladd = backadrr + str(yourip)
 
     r = requests.get(fulladd)
     data = r.json()
     ipcountry = data["country"]
-
+    # for test only uncomment
+    #ipcountry = "CH"
 
 def up():
     with open(filename, "rb") as f:
         dbx.files_upload(f.read(), "/" + filename)
-
+    
+    with open(countryfile, "rb") as f:
+        dbx.files_upload(f.read(), "/" + countryfile)
 
 def down():
     dbx.files_download_to_file(filename, "/" + filename)
-
+    dbx.files_download_to_file(countryfile, "/" + countryfile)
 
 def deleter():
     dbx.files_delete("/" + filename)
+    dbx.files_delete("/" + countryfile)
 
 
 def main():
-    global counter
-    try:
-        down()
-    except:
-        pass
+    global counter,country, maxcountry
+
+    down()
 
     f = open(filename, "r")
     value = f.read()
 
     counter = int(value) + 1
+    
+    with open(countryfile) as f:
+        country = [line.rstrip() for line in f]
+    
+    maxcountry = max(set(country), key=country.count)
+             
+    country = list(dict.fromkeys(country))
+    country = ' '.join(map(str, country))
 
-    try:
-        deleter()
-    except:
-        pass
+    deleter()
 
     f = open(filename, "w")
     f.write(str(counter))
     f.close()
-
+    
+    f= open(countryfile, "a")
+    f.write("\n")
+    f.write(ipcountry)
+    f.close()
+  
     up()
 
 
@@ -72,12 +87,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # take the visitor ip
-    # yourip = request.remote_addr
     yourip = request.environ.get("HTTP_X_FORWARDED_FOR", request.remote_addr)
-
+    
+    geo()
+  
     main()
 
-    geo()
 
     random_number = random.randint(1, 1000)
     return render_template(
@@ -86,4 +101,6 @@ def index():
         counter=counter,
         yourip=yourip,
         ipcountry=ipcountry,
+        country=country,
+        maxcountry=maxcountry
     )
